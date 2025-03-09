e54822ed7df18a473c5236ea2ea44178064c6b1e
SariYona <sari100375@gmail.com>
2025-03-09 20:42:37 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import renderApi from '@api/render-api';
+
+
+const app = express()
+const port = 3001
+app.use(bodyParser.json())
+app.listen(port, ()=>{
+    console.log(`The running is sucses http://localhost:${port}`);
+})
+
+
+app.get('/hello', (req, res) => {
+    res.send('👍✌❤😂😊🌹👏')
+})
+
+
+
+renderApi.auth('rnd_FBwW0y9CeHQyL985kYc9LO6w1mWb');
+
+
+renderApi.listServices({name: '', includePreviews: 'true', limit: '20'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+
+
