f0237b121dd3e455827328db8508ad259e79b4fd
SariYona <sari100375@gmail.com>
2025-03-09 21:00:10 +0200

    init commit

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ app.listen(port, ()=>{
 })
 
 
-app.get('/hello', (req, res) => {
-    res.send('👍✌❤😂😊🌹👏')
-})
 
 
 
 renderApi.auth('rnd_FBwW0y9CeHQyL985kYc9LO6w1mWb');
 
 
+app.get('/', (req, res) => {
+    res.send('👍✌❤😂😊🌹👏')
+})
 renderApi.listServices({name: '', includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
